d0c4ccbc96e68d40257e2e73db0737115460e77e
alfred kivuva <kivuvakioko97@gmail.com>
2023-12-14 01:17:55 +0300

    completed assigment

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
 // Code your solution in this file!
+
+ let drivers=['Antonia', 'Nuru', 'Amari', 'Mo'];
+
+  const returnFirstTwoDrivers = function(drivers) {
+    return drivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers=function(drivers){
+    return drivers.slice(drivers.length-2,drivers.length);
+  }
+  const selectingDrivers=[returnFirstTwoDrivers, returnLastTwoDrivers];
+
+  function createFareMultiplier(multiplier) {
+    return function (fare) {
+      return multiplier * fare;
+    };
+};
+
+  const fareDoubler = createFareMultiplier(2)
+  const fareTripler = createFareMultiplier(3)
+  fareDoubler(fare);
+  fareTripler(fare);
+
+    function selectDifferentDrivers(drivers, selectingFunction) {
+        return selectingFunction(drivers);
+  };
